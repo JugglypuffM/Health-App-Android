873fc8068cca9328256a45eb23fe6a734cfe4848
JugglypuffM <stas.klinin@gmail.com>
2024-10-25 17:18:43 +0500

    изменение: toString в BasicUserData и добавление проверки DataService в TestGRPC

--- app/src/main/kotlin/TestGRPC.kt
+++ app/src/main/kotlin/TestGRPC.kt
@@ -1,11 +1,16 @@
 import auth.Authenticator
 import auth.GrpcAuthenticator
+import data.DataRequester
+import data.GrpcDataRequester
 
 suspend fun main() {
     val authenticator: Authenticator = GrpcAuthenticator()
+    val dataRequester: DataRequester = GrpcDataRequester()
     println(authenticator.register("stas", "jpf", "123456"))
     println(authenticator.register("stas", "jpf", "123465"))
     println(authenticator.login("jpf", "123456"))
     println(authenticator.register("stas", "jpf", "1234"))
     println(authenticator.login("jpf", "1234"))
+    println(dataRequester.getBasicUserData("jpf", "12345"))
+    println(dataRequester.getBasicUserData("jpf", "123456"))
 }
