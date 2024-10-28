9ac98c27db7805de5c829eb29e15cc0ed25476b0
syoumzic <syoumzic@gmail.com>
2024-10-29 00:13:30 +0500

    Реализация локального хранилища

--- app/src/main/kotlin/utils/LocalStorage.kt
+++ app/src/main/kotlin/utils/LocalStorage.kt
@@ -1,5 +1,7 @@
 package utils
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
 import domain.Either
 import domain.User
@@ -7,28 +9,63 @@ import domain.User
 /**
  * Локальное хранилище
  */
-//TODO реализовать базу данных для хранения и извлечения пользователей
 class LocalStorage {
     class UserNotFoundException(message: String) : Exception(message)
+
+    private lateinit var sharedPreferences: SharedPreferences
+
+    private val LOGIN = "login"
+    private val PASSWORD = "password"
+    private val TABLE_NAME = "password"
+
+
+    private fun fromNullable(value: String?): Either<Throwable, String>{
+        return Either.fromNullable(value, UserNotFoundException("User has incorrectly saved"))
+    }
+
+    private fun get(key: String): String? {
+        return sharedPreferences.getString(key, null)
+    }
 
     /**
      * Загрузить пользователя
      */
-    fun loadUser(): Either<Throwable, User> {
-        return Either.Right(User("John Doe", "john_doe", "password123"))
+    suspend fun loadUser(): Either<Throwable, User> {
+        Log.d("MYDB", "Loading user...")
+        return fromNullable(get(LOGIN)).flatMap { login ->
+            fromNullable(get(PASSWORD)).map { password ->
+                User(null, login, password)
+            }
+        }
     }
 
     /**
      * Сохранить пользователя
      */
     fun saveUser(user: User) {
-        Log.d("DB", "User saved: $user")
+        val editor = sharedPreferences.edit()
+
+        editor.putString("LOGIN", user.login)
+        editor.putString("PASSWORD", user.password)
+
+        editor.apply()
+        Log.d("MYDB", "User saved: $user")
     }
 
     /**
      * Удалить пользователя
      */
     fun dropUser() {
-        Log.d("DB", "user dropped")
+        val editor = sharedPreferences.edit()
+        editor.clear() // Удаляет все данные
+        editor.apply() // Применяет изменения
+        Log.d("MYDB", "user dropped")
+    }
+
+    /**
+     * Установить контекст
+     */
+    fun setContext(context: Context) {
+        sharedPreferences = context.getSharedPreferences(TABLE_NAME, Context.MODE_PRIVATE)
     }
 }
