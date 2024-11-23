85f207d7a11de3f2be1cb6d8320516b02edf1aa5
syoumzic <rofetofe@mail.ru>
2024-11-23 15:42:08 +0500

    feat: добавил описание для йоги и бега

--- app/src/main/kotlin/domain/training/Training.kt
+++ app/src/main/kotlin/domain/training/Training.kt
@@ -7,14 +7,29 @@ import kotlin.time.Duration
  * Информация о тренировке
  * Специфична для каждого типа тренировки
  */
-sealed class Training(val date: LocalDate, val duration: Duration) {
+sealed class Training(
+    val title: String,
+    val description: String,
+    val date: LocalDate,
+    val duration: Duration
+) {
     /**
      * Информация о йоге
      */
-    class Yoga(date: LocalDate, duration: Duration) : Training(date, duration)
+    class Yoga(date: LocalDate, duration: Duration) : Training(
+        title = "Йога",
+        description = "Йога помогает улучшить гибкость, снять стресс и укрепить мышечный корсет",
+        date=date,
+        duration=duration
+    )
 
     /**
      * Информация о беге
      */
-    class Jogging(date: LocalDate, duration: Duration, val distance: Double) : Training(date, duration)
+    class Jogging(date: LocalDate, duration: Duration, val distance: Double) : Training(
+        title="Бег",
+        description="Бег способствует укреплению сердечно-сосудистой системы, улучшению выносливости и сжиганию лишних калорий",
+        date=date,
+        duration=duration
+    )
 }
