f2d1f0060b1ef342559714744953fccc43d263be
syoumzic <rofetofe@mail.ru>
2024-12-06 20:18:09 +0500

    fix: Исправил йогу

--- app/src/main/kotlin/domain/training/TrainingActions.kt
+++ app/src/main/kotlin/domain/training/TrainingActions.kt
@@ -9,17 +9,16 @@ import java.io.Serializable
 enum class TrainingActions(var actions: List<TrainingAction>): Serializable {
     Yoga (
         listOf(
-            TrainingAction("Подготовьтесь", 12000), // Подготовка
-            TrainingAction("Перерыв", 10000),        // Временной интервал
-            TrainingAction("Поза 1: Гора", 62000),   // Первая поза
-            TrainingAction("Перерыв", 10000),        // Временной интервал
-            TrainingAction("Поза 2: Дерево", 62000), // Вторая поза
-            TrainingAction("Перерыв", 10000),        // Временной интервал
-            TrainingAction("Поза 3: Воин", 62000),   // Третья поза
-            TrainingAction("Перерыв", 10000),        // Временной интервал
-            TrainingAction("Поза 4: Кобра", 62000),  // Четвертая поза
-            TrainingAction("Перерыв", 10000),        // Временной интервал
-            TrainingAction("Завершение занятия", 12000) // Завершение
+            TrainingAction("Подготовьтесь", 12000),
+            TrainingAction("Поза 1: Гора", 62000),
+            TrainingAction("Перерыв", 10000),
+            TrainingAction("Поза 2: Дерево", 62000),
+            TrainingAction("Перерыв", 10000),
+            TrainingAction("Поза 3: Воин", 62000),
+            TrainingAction("Перерыв", 10000),
+            TrainingAction("Поза 4: Кобра", 62000),
+            TrainingAction("Перерыв", 10000),
+            TrainingAction("Завершение занятия", 12000)
         )
     );
 }
