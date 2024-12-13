44d4e0416e402e26c962495cfaff8a09be3a9cd1
Nikita <vip.nikita.bulygin@gmail.com>
2024-12-13 18:19:10 +0500

    Рефактор

--- app/src/main/kotlin/KotlinAndroidApp/HomeScreenActivity.kt
+++ app/src/main/kotlin/KotlinAndroidApp/HomeScreenActivity.kt
@@ -1,13 +1,10 @@
 package KotlinAndroidApp
 
-import android.app.Application
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.project.kotlin_android_app.R
 import domain.training.Training
@@ -32,9 +29,7 @@ class HomeScreenActivity : AppCompatActivity() {
         btnPrevious = findViewById(R.id.btnPrevious)
         btnNext = findViewById(R.id.btnNext)
 
-        // Используем ViewModelProvider.Factory для передачи Application
-        viewModel = ViewModelProvider(this, HomeScreenViewModelFactory(application))
-            .get(HomeScreenViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(HomeScreenViewModel::class.java)
 
         viewModel.currentTraining.observe(this, { training ->
             updateUI(training)
@@ -57,21 +52,5 @@ class HomeScreenActivity : AppCompatActivity() {
             is Training.Yoga -> workoutImage.setImageResource(R.drawable.ic_yoga)
             is Training.Jogging -> workoutImage.setImageResource(R.drawable.ic_running)
         }
-    }
-// Переход на тренировку
-//    private fun startWorkout(trainingId: Int) {
-//        val intent = Intent(this, TrainingActivity::class.java)
-//        intent.putExtra("TRAINING_ID", trainingId)
-//        startActivity(intent)
-//    }
-}
-
-class HomeScreenViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(HomeScreenViewModel::class.java)) {
-            @Suppress("UNCHECKED_CAST")
-            return HomeScreenViewModel(application) as T
-        }
-        throw IllegalArgumentException("Unknown ViewModel class")
     }
 }
--- app/src/main/kotlin/KotlinAndroidApp/MainApplication.kt
+++ app/src/main/kotlin/KotlinAndroidApp/MainApplication.kt
@@ -2,16 +2,15 @@ package KotlinAndroidApp
 
 import android.app.Application
 import android.util.Log
+import auth.Authenticator
 import auth.AuthenticatorStub
+import auth.GrpcAuthenticator
+import com.project.kotlin_android_app.BuildConfig
 import data.DataRequesterStub
+import data.GrpcDataRequester
 import utils.UserSerializer
 import utils.Validator
 import viewmodel.ViewModel
-import domain.training.Training
-import domain.training.Training.Yoga
-import domain.training.Training.Jogging
-import kotlinx.datetime.LocalDate
-import kotlin.time.Duration.Companion.minutes
 
 /**
  * Класс, представляющий глобальное состояние приложения.
@@ -20,15 +19,10 @@ class MainApplication : Application() {
     lateinit var viewModel: ViewModel
         private set
 
-    val trainings: List<Training> = listOf(
-        Yoga(LocalDate(2023, 10, 1), 60.minutes),
-        Jogging(LocalDate(2023, 10, 2), 30.minutes, 5.0)
-    )
-
     override fun onCreate() {
         super.onCreate()
 
-        Log.d("ATH", "Application started")
+        Log.d("ATH", "server address: ${BuildConfig.serverAddress}, port: ${BuildConfig.serverPort}")
         viewModel = ViewModel(
             UserSerializer(applicationContext),
             AuthenticatorStub(),
