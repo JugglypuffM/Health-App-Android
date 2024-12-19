addb678956cc5b511da4a45ffffbb7578d98aaac
syoumzic <rofetofe@mail.ru>
2024-12-19 16:14:36 +0500

    feat: добавлен tts на тренеровки

--- app/src/main/kotlin/KotlinAndroidApp/TrainingActivity.kt
+++ app/src/main/kotlin/KotlinAndroidApp/TrainingActivity.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,11 +17,14 @@ import androidx.appcompat.app.AppCompatActivity
 import com.project.kotlin_android_app.R
 import domain.training.TrainingActions
 import java.time.Duration
+import java.util.Locale
 
 /**
  * Активность тренировки (таймер на выполнение списка действий тренировки)
  */
-class TrainingActivity : AppCompatActivity() {
+class TrainingActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    private lateinit var tts: TextToSpeech
 
     @SuppressLint("DefaultLocale")
     @RequiresApi(Build.VERSION_CODES.S)
@@ -44,6 +49,7 @@ class TrainingActivity : AppCompatActivity() {
             updateActivity = { action: TrainingAction ->
                 titleText.text = action.title
                 image.setImageResource(action.imageSource)
+                speak(action.title);
             },
             finish = {
                 //save training to history
@@ -54,6 +60,30 @@ class TrainingActivity : AppCompatActivity() {
 
         startStopButton.setOnClickListener{
             timerChain.cancel()
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Установка языка
+            val result = tts.setLanguage(Locale("ru", "RU"))
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.d("ERRM", "Язык не поддерживается или отсутствуют данные.")
+            }
+        } else {
+            Log.d("ERRM", "Ошибка инициализации TTS")
+        }
+    }
+
+    fun speak(message: String){
+        tts.speak(message, TextToSpeech.QUEUE_FLUSH, null, null)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (::tts.isInitialized) {
+            tts.stop()
+            tts.shutdown()
         }
     }
 }
